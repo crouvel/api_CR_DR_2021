bf2802b42b8fc8a7ee56702c18ac9e162829cf75
Clarence Rouvel <clarence.rouvel@gmail.com>
2021-07-23 02:28:00 +0200

    clear code

--- DAO/mapjson.js
+++ DAO/mapjson.js
@@ -15,7 +15,10 @@ const mapjson = async (JSobjectjson) => {
         
         
         //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
-        connection.query('INSERT INTO Records (recordid,gc_obo_nature_c,gc_obo_gare_origine_r_name,date,gc_obo_nom_recordtype_sc_c,gc_obo_type_c,gc_obo_gare_origine_r_code_uic_c) VALUES ?', [values], function(err,result) {
+        connection.query('INSERT INTO Records (recordid,gc_obo_nature_c,gc_obo_gare_origine_r_name,date,gc_obo_nom_recordtype_sc_c,gc_obo_type_c,gc_obo_gare_origine_r_code_uic_c) VALUES ?', 
+        [values], 
+        
+        function(err,result) {
           if(err) {
              res.send('Error');
           }
