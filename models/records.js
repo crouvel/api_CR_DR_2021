82eee26f95fb9605defe37e5a8577340b5a70449
Clarence Rouvel <clarence.rouvel@gmail.com>
2021-08-03 01:17:39 +0200

    correction

--- models/records.js
+++ models/records.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     date: DataTypes.DATE,
     gc_obo_nom_recordtype_sc_c: DataTypes.STRING,
     gc_obo_type_c: DataTypes.STRING,
-    gb_obo_gare_origine_r_code_uic_c: DataTypes.STRING
+    gc_obo_gare_origine_r_code_uic_c: DataTypes.STRING
   }, {});
   Records.associate = function(models) {
     // associations can be defined here
